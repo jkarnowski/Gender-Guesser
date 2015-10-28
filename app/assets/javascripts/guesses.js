dabc3381cd6c84d4b8b95db78d7cfe045b256fbc
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-28 08:53:53 -0700

    update query to find height and weight

--- app/assets/javascripts/guesses.js
+++ app/assets/javascripts/guesses.js
@@ -1,2 +1,36 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+	console.log("ready inside guesses")
+
+	// buttonListener();
+})
+
+var buttonListener = function(){
+	console.log("inside the buttonListener")
+
+	$('.clicked').on('click', function(event){
+		console.log(this)
+		event.preventDefault();
+		// console.log("preventing....default")
+
+		var url =  '/guesses'
+		var method =  'POST'
+		var dataType =  "jsonp"
+
+		$.ajax({
+			url: url,
+			beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+			method: method,
+			dataType: dataType
+		})
+		.success(function(sResponse){
+			console.log(sResponse)
+		})
+		.error(function(sResponse){
+			console.log(sResponse)
+			console.log("FAILING")
+		});
+	})
+}
--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,55 +1,35 @@
 class GuessesController < ApplicationController
   def index
-  	@persons = Person.all 
+  	@persons = Person.all
   end
 
   def new
   end
 
   def create
-    temporary_person = Person.new(person_params)
+    person = Person.new(person_params)
+    p "person from params:"
+    p person 
 
-    height = person_params[:height]
-    weight = person_params[:weight]
+    check_height = person.height
+    check_weight = person.weight
 
-    p height 
-    p 'person inside the CREATE'
-    p temporary_person
-    # p "going into the method........."
+    find_person = Person.where("height = ? AND weight = ?", check_height, check_weight).sample
+    p "find person in database:"
+    p find_person
 
-    # find_gender_for_height_weight
-    temporary_person = Person.where({height: height, weight: weight}).sample
-
-    if temporary_person.nil?
-      p "inside the IF"
-      p temporary_person
-      p "*" * 100
-      p params
-      p "*" * 100
-      gender = ["Female", "Male"].sample
-      p "random gender: "
-      p gender 
-      @person = Person.create(weight: params[:weight], height: params[:height], gender: gender)
-      p "inside IF and person is:"
-      @person 
+    if find_person.gender != nil
+      p "inside the IF:"
+      @guessing_gender = find_person.gender
     else
-      # gender = ["Female", "Male"].sample
-      # gender = "MALESYYYYY"
-      p "inside the ELSE:"
-      p params
-      @person = temporary_person
+      p "inside the ELSE"
+      gender = ["male", "female"].sample
+      guessed_gender = Person.create(height: check_height, weight: check_weight, gender: gender)
+      p guessed_gender.gender 
     end
 
-    p "our person with GENDER IS:"
-    @person
   end
 
-  def find_gender_for_height_weight
-
-    # check_height = person_params[:height]
-    # check_weight = person_params[:weight]
-
-  end
 
   private
   def person_params
@@ -83,5 +63,29 @@ end
 	#65, 145, female && 65, 145, male BOTH exist but different % of times true
 	# return higher %
 
-# 
 
+
+
+    # if person == Person.where(height: check_height, weight: check_weight)
+    #   if Person.where(gender != nil)
+    #     p "inside IF IF"
+    #     @existing_person_gender = person.gender
+    #     p @existing_person_gender
+    #   else
+    #     gender = ["male", "female"].sample
+    #     person.update_attributes(gender: gender)
+    #     p person.gender
+    #   end
+    #   p "inside the IF, person: "
+    #   p person
+    #   redirect_to :back
+    # else
+    #   p "inside the ELSE, params: "
+    #   p params
+    #   redirect_to :back
+    # end
+
+    # find_gender_for_person = Person.where(height: person.height, weight: person.weight).sample
+    # p find_gender_for_person.gender
+
+    # redirect_to :back
