b976bdc9a314134469a7b5f83e32b0ef5419db28
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-28 10:24:28 -0700

    add factory girl

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:person) {Person.new(height: "65", weight: "166")}
+
+	it 'is invalid without a height' do
+
+	end
+	it 'is invalid without weight'
+	it 'includes gender'
 end
+
+
+
+
+	validates :height, presence: true
+	validates :weight, presence: true
