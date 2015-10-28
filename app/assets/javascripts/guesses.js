5400997fdafdb78ae18106c7e95ead9e7322547a
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-28 09:37:39 -0700

    update index and new to display guessed gender

--- app/assets/javascripts/guesses.js
+++ app/assets/javascripts/guesses.js
@@ -9,28 +9,5 @@ $(document).ready(function(){
 
 var buttonListener = function(){
 	console.log("inside the buttonListener")
-
-	$('.clicked').on('click', function(event){
-		console.log(this)
-		event.preventDefault();
-		// console.log("preventing....default")
-
-		var url =  '/guesses'
-		var method =  'POST'
-		var dataType =  "jsonp"
-
-		$.ajax({
-			url: url,
-			beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-			method: method,
-			dataType: dataType
-		})
-		.success(function(sResponse){
-			console.log(sResponse)
-		})
-		.error(function(sResponse){
-			console.log(sResponse)
-			console.log("FAILING")
-		});
-	})
+	
 }
--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -4,30 +4,34 @@ class GuessesController < ApplicationController
   end
 
   def new
+    find_person
   end
 
-  def create
+  def find_person
     person = Person.new(person_params)
-    p "person from params:"
-    p person 
+    p person_params
+
+    p "person is:"
+    person 
 
     check_height = person.height
     check_weight = person.weight
 
     find_person = Person.where("height = ? AND weight = ?", check_height, check_weight).sample
-    p "find person in database:"
-    p find_person
 
     if find_person.gender != nil
-      p "inside the IF:"
-      @guessing_gender = find_person.gender
+      @guessed_gender = find_person.gender
+      @person = Person.new(height: check_height, weight: check_weight, gender: @guessed_gender)
     else
-      p "inside the ELSE"
       gender = ["male", "female"].sample
-      guessed_gender = Person.create(height: check_height, weight: check_weight, gender: gender)
-      p guessed_gender.gender 
+      @person = Person.new(height: check_height, weight: check_weight, gender: gender)
     end
 
+    return @person 
+
+  end
+
+  def create
   end
 
 
@@ -62,30 +66,3 @@ end
 #else
 	#65, 145, female && 65, 145, male BOTH exist but different % of times true
 	# return higher %
-
-
-
-
-    # if person == Person.where(height: check_height, weight: check_weight)
-    #   if Person.where(gender != nil)
-    #     p "inside IF IF"
-    #     @existing_person_gender = person.gender
-    #     p @existing_person_gender
-    #   else
-    #     gender = ["male", "female"].sample
-    #     person.update_attributes(gender: gender)
-    #     p person.gender
-    #   end
-    #   p "inside the IF, person: "
-    #   p person
-    #   redirect_to :back
-    # else
-    #   p "inside the ELSE, params: "
-    #   p params
-    #   redirect_to :back
-    # end
-
-    # find_gender_for_person = Person.where(height: person.height, weight: person.weight).sample
-    # p find_gender_for_person.gender
-
-    # redirect_to :back
