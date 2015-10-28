4b9036ac751a1f322b3c2bd08294ea2e32c8cd83
jkarnowski <jaclyn.karnowski@gmail.com>
2015-10-28 11:39:02 -0700

    add model tests that pass

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,13 +4,21 @@ require 'rails_helper'
 RSpec.describe Person, type: :model do
 
 	before :each do
-		@person = Person.new(height: '65', weight: '155', gender: 'male')
+		@testing = Person.new(height: '65', weight: '155', gender: 'male')
 	end
 
 	describe 'instantiation' do
 
 		it 'instantiates a person' do
-			expect(@person.height).to eq('65')
+			expect(@testing.height).to eq('65')
+		end
+	end
+
+	describe 'validations' do 
+
+		it 'is not a valid person without weight' do
+		person = FactoryGirl.build(:person, height: '67', weight: nil)
+		expect(person).not_to be_valid
 		end
 	end
 
